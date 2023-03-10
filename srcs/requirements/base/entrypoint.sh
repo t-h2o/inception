c19b03ff03794e25346136bcc82b1ad16a84a696
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-03-10 16:13:31 +0100

    base: create a simple configuration file

--- srcs/requirements/base/entrypoint.sh
+++ srcs/requirements/base/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 echo ${MYNAME} > file_created_by_base_image.txt
+
+printf "\nConcatenate the configuration file into this file:\n\n" >> file_created_by_base_image.txt
+
+cat /etc/myconfig.conf >> file_created_by_base_image.txt
