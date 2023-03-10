14875295ce3edf428a9ebd1121e80e3a17260f66
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-03-10 16:21:07 +0100

    base: substitutes environment in a config file

--- srcs/requirements/base/entrypoint.sh
+++ srcs/requirements/base/entrypoint.sh
@@ -5,3 +5,7 @@ echo ${MYNAME} > file_created_by_base_image.txt
 printf "\nConcatenate the configuration file into this file:\n\n" >> file_created_by_base_image.txt
 
 cat /etc/myconfig.conf >> file_created_by_base_image.txt
+
+printf "\nSubstitutes environment variables:\n\n" >> file_created_by_base_image.txt
+
+envsubst < /etc/myconfig.conf >> file_created_by_base_image.txt
