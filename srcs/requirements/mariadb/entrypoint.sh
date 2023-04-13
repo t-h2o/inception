59c8a5ac818d8c613fc14e3aa9397a86819b5760
Theo Grivel <grivel.theo@protonmail.com>
2023-04-13 14:38:31 +0200

    marrria wordpress and volume
    * create the volume basedata
      for saving maria data
    * marrria: adapt entrypoint to setup
      wordpress
    * marrria: update entrypoint script
    
    1. install the db if the db is already
       installed, that just print a message
       else install the db
    
    2. launch deamon in the backgroud sleep a
       second to be sure the deamon was well
       launched
    
    3. check if the wordpress database is
       already installed else install it and
       its user
    
    4. run the deamon in the foreground

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
+
+set -m # Enable Job Control
+
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-mariadbd -u mysql
+mariadbd -u mysql &
+
+sleep 1 # to be sure the deamon is launched, not the better pratice... to improve
+
+printf "mariadbd launched\n"
+
+if mariadb-show | grep wordpress>/dev/null ; then
+	printf "wordpress database and user already installed...\n"
+else
+	printf "create wordpress database and user...\n"
+	mariadb -u root --password='' << eof
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'philippe.incepnet' IDENTIFIED BY '1234';
+FLUSH PRIVILEGES;
+eof
+	printf "wordpress database and user installed...\n"
+fi
+
+fg
 
 # into another bash session,
 # connect into the DB,
-# apply this command
-# GRANT ALL on root.* to root@_gateway IDENTIFIED BY '1234';
-# this command enable the root user
-# to connect into the docker image from the docker host
-# _gateway is an alias as localhost
+#
+# apply these commands:
+# CREATE DATABASE wordpress;
+# GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'philippe.incepnet' IDENTIFIED BY '1234';
+# FLUSH PRIVILEGES;
+# EXIT
