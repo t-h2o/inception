98179aae63a3d4cef2c8db7a8582248df934a8be
|°_°| <grivel.theo@protonmail.com>
2023-04-15 21:39:33 +0200

    create-env.sh: pwgen
    * check if pwgen is installed
    * set an alias to pwgen
    
    https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script

--- create-env.sh
+++ create-env.sh
@@ -4,6 +4,11 @@ ENVIRONMENT_FILE="srcs/.env"
 
 LOGIN="$(whoami)"
 
+if ! hash pwgen 2> /dev/null ; then
+	printf "pwgen is not installed\n"
+	exit 1
+fi
+
 if [ -f ${ENVIRONMENT_FILE} ]; then
 	printf "the .env file already exist, replace it ?\n[Y/n]: "
 	read answer
@@ -12,11 +17,13 @@ if [ -f ${ENVIRONMENT_FILE} ]; then
 	fi
 fi
 
+alias pwgen="pwgen --ambiguous --capitalize --secure 20 1"
+
 cat > ${ENVIRONMENT_FILE} << eof
 DOMAIN_NAME=${LOGIN}.42.fr
 
 DATABASE_DATABASE=wordpress
 DATABASE_USER_NAME=wordpress
-DATABASE_USER_PASSWORD=$(pwgen --ambiguous --capitalize --secure 20 1)
-DATABASE_ROOT_PASSWORD=$(pwgen --ambiguous --capitalize --secure 20 1)
+DATABASE_USER_PASSWORD=$(pwgen)
+DATABASE_ROOT_PASSWORD=$(pwgen)
 eof
