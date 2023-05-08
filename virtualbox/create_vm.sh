302c1df7c0f9767e93741690c710dab4342d17ac
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2023-05-08 15:56:44 +0200

    create vm and setup the OS
    * interactiv menu for virtualBox
      - setup the vm
      - attach the image
      - download the iso
      - boot the vm
    
    * install script for Arch Linux
      - ask a username
      - partition the disk
      - install
        - docker
        - openssh
        - misc tools
      - enable services
        - sshd
        - docker
      - create an user
    
    TODO:
    - find better sentences
    - improve the interactiv menu
    - explain how use the interactiv menu

---
+++ virtualbox/create_vm.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# vboxmanage manual
+# https://www.virtualbox.org/manual/ch08.html
+
+# remove from the list
+# VBoxManage list vms
+# VBoxManage unregistervm <ID>|<NAME>
+
+VM_PATH="${HOME}/goinfre/my_vm_created_by_cli"
+VM_NAME="vmCreatedByCLI"
+VDI_FILE="${VM_PATH}/Archlinux_64.vdi"
+ARCHISO_FILE="${VM_PATH}/archlinux_bootable.iso"
+ARCHISO_VERSION="2023.03.01"
+
+create_vm () {
+	VBoxManage createvm --name "${VM_NAME}" --basefolder="${VM_PATH}" --ostype Archlinux_64 --register
+	VBoxManage modifyvm "${VM_NAME}" --nic1 bridged --bridgeadapter1 eth0 --memory=1024 --vram=16 --pae=off --rtcuseutc=on
+}
+
+create_vdi () {
+	# For list hdds: vboxmanage list hdds
+	# For delete vdi: vboxmanage closemedium disk eaf35acc-b45b-4b8f-80f0-6877b8621311 --delete
+	VBoxManage createhd --format VDI --size 16384 --variant Fixed --filename "${VDI_FILE}"
+}
+
+create_vm_sata () {
+	VBoxManage storagectl "${VM_NAME}" --name "SATA Controller" --add sata --bootable on
+	VBoxManage storageattach "${VM_NAME}" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${VDI_FILE}"
+}
+
+download_archlinux_iso () {
+	if [ -f "${ARCHISO_FILE}" ]; then
+		echo "${ARCHISO_FILE} already exists"
+	else
+		curl https://theswissbay.ch/archlinux/iso/"${ARCHISO_VERSION}"/archlinux-x86_64.iso --output "${ARCHISO_FILE}"
+	fi
+}
+
+attach_archlinux_iso () {
+	VBoxManage storagectl "${VM_NAME}" --name "IDE Controller" --add ide
+	VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "${ARCHISO_FILE}"
+}
+
+remove_archlinux_iso () {
+	VBoxManage storageattach "${VM_NAME}" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium none
+}
+
+start_vm () {
+	# headless means, without graphic interface
+	VBoxManage startvm "${VM_NAME}" # --type headless
+}
+
+# $1: step name
+step_menu () {
+	echo "$1"
+	printf "jump [y/N/exit]: "; read answer
+	if [ "${answer}" == "" ]; then
+		echo "continue"
+		return 0
+	elif [ "${answer}" == "n" ]; then
+		echo "continue"
+		return 0
+	elif [ "${answer}" == "y" ]; then
+		echo "jump"
+		return 1
+	elif [ "${answer}" == "exit" ]; then
+		echo "exit"
+		exit 0
+	fi
+}
+
+setup_the_vm () {
+	# generate the folder
+	mkdir -p "${VM_PATH}"
+
+	step_menu "create the vm"
+
+	# create the vm
+	if [ $? -eq 0 ]; then
+		create_vm
+	fi
+
+	step_menu "download_archlinux_iso"
+
+	# download GNU/Linux iso
+	if [ $? -eq 0 ]; then
+		download_archlinux_iso
+	fi
+
+	step_menu "attach_archlinux_iso"
+
+	# set a bootable image
+	if [ $? -eq 0 ]; then
+		attach_archlinux_iso
+	fi
+
+	step_menu "create_vdi"
+
+	# create a vdi
+	if [ $? -eq 0 ]; then
+		create_vdi
+	fi
+
+	step_menu "create_vm_sata"
+
+	# create a sata
+	if [ $? -eq 0 ]; then
+		create_vm_sata
+	fi
+
+	step_menu "start_vm"
+
+	if [ $? -eq 0 ]; then
+		start_vm
+	fi
+
+}
+
+main_menu () {
+	printf "main menu:\n1: setup the vm\n2: list vms\n3: exit\n4: start the vm\n5: generate info-vm.txt\n6: remove archlinux iso\n7: delete the vm\n[1-7]: "; read answer
+	if [ "${answer}" == "1" ]; then
+		return 1
+	elif [ "${answer}" == "2" ]; then
+		return 2
+	elif [ "${answer}" == "3" ]; then
+		return 3
+	elif [ "${answer}" == "4" ]; then
+		return 4
+	elif [ "${answer}" == "5" ]; then
+		return 5
+	elif [ "${answer}" == "6" ]; then
+		return 6
+	elif [ "${answer}" == "7" ]; then
+		return 7
+	else
+		printf "\ntry again\n\n"
+		main_menu
+	fi
+}
+
+main () {
+	main_menu
+
+	main_answer=$?
+	if [ ${main_answer} -eq 1 ]; then
+		setup_the_vm
+	elif [ ${main_answer} -eq 2 ]; then
+		VBoxManage list vms
+	elif [ ${main_answer} -eq 3 ]; then
+		exit 0
+	elif [ ${main_answer} -eq 4 ]; then
+		start_vm
+	elif [ ${main_answer} -eq 5 ]; then
+		VBoxManage showvminfo ${VM_NAME} > info-vm.txt
+	elif [ ${main_answer} -eq 6 ]; then
+		remove_archlinux_iso
+	elif [ ${main_answer} -eq 7 ]; then
+		VBoxManage unregistervm --delete ${VM_NAME}
+	fi
+
+	main
+}
+
+main
---
+++ virtualbox/install.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+
+partition_the_disk () {
+	# overwrite the disk
+	# create two partitions
+	# 1 swap
+	# 2 linux filesystem
+	gdisk /dev/sda << eof
+o
+y
+n
+
+
++1GiB
+8200
+p
+n
+
+
+
+8300
+p
+w
+y
+eof
+}
+
+format_the_partitions () {
+	mkswap -L THE_SWAP /dev/sda1
+	mkfs.ext4 -L THE_ROOT /dev/sda2
+}
+
+mount_the_file_systems () {
+	swapon /dev/sda1
+	mount /dev/sda2 /mnt
+}
+
+install_essential_packages () {
+	pac="base"
+	pac="$pac base-devel"
+	pac="$pac linux-zen"
+	pac="$pac linux-firmware"
+
+	pac="$pac dosfstools"
+	pac="$pac e2fsprogs"
+
+	pac="$pac grub"
+
+	pac="$pac networkmanager"
+
+	pac="$pac git"
+	pac="$pac vim"
+	pac="$pac zsh"
+	pac="$pac tmux"
+	pac="$pac tree"
+	pac="$pac kitty"
+	pac="$pac neofetch"
+
+	pac="$pac man-db"
+	pac="$pac man-pages"
+
+	pacstrap /mnt $pac
+}
+
+fstab () {
+	genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+chroot () {
+	region="Europe"
+	city="Zurich"
+
+	myhostame="panic"
+	mypw="1234"
+
+	arch-chroot /mnt << eof
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo $myhostame > /etc/hostname
+
+groupadd wheel
+useradd -m $myuser
+usermod -aG wheel $myuser
+
+yes $mypw | passwd
+yes $mypw | passwd $myuser
+
+EDITOR="sed -i 's/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/  %wheel ALL=(ALL:ALL) NOPASSWD: ALL/'" visudo
+eof
+}
+
+bootloader () {
+	arch-chroot /mnt << eof
+grub-install --force --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+eof
+}
+
+enable_services () {
+	arch-chroot /mnt << eof
+pacman -S --noconfirm openssh docker docker-compose ansible
+systemctl enable sshd
+systemctl enable docker
+systemctl enable NetworkManager
+usermod -aG docker ${myuser}
+su ${myuser}
+cd
+git clone https://github.com/t-h2o/inception
+git clone https://github.com/t-h2o/ansible-my-conf
+ssh-keygen -t ed25519 -f /home/${myuser}/.ssh/id_ed25519 -P ""
+cat /etc/ssh/ssh_host_ed25519_key.pub >> /home/${myuser}/.ssh/known_hosts
+cat /home/${myuser}/.ssh/id_ed25519.pub >> /home/${myuser}/.ssh/authorized_keys
+exit
+eof
+}
+
+ask_user_name () {
+	printf "username: "
+	read myuser
+
+	if [ "$(echo ${myuser} | wc -w)" -ne 1 ]; then
+		printf "not valid username\n"
+		ask_user_name
+	fi
+}
+
+main () {
+	ask_user_name
+
+	printf "partition the disk...\n"
+	partition_the_disk
+
+	printf "format the partitions...\n"
+	format_the_partitions
+
+	printf "mount the file systems...\n"
+	mount_the_file_systems
+
+	printf "install essential packages...\n"
+	install_essential_packages
+
+	printf "fstab...\n"
+	fstab
+
+	printf "chroot...\n"
+	chroot
+
+	printf "set the bootloader...\n"
+	bootloader
+
+	printf "enable services...\n"
+	enable_services
+
+	printf "shutdown and reboot without the iso file.\n"
+}
+
+main
